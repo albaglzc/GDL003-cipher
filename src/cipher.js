e5b222f23d735f03693e485adcb2003149fb8918
alba <albaglzc@gmail.com>
2019-06-06 17:53:17 -0500

    contenedores y botones

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,44 @@
+/*
+//texto que introduce el usuario en el formulario
+let textoDelUsuario = texto.value;
+//número de espacios que se recorrerá el texto
+let shift;
+//función cifrar o codificar texto:
+function encrypt() {
+    let result = "";
+    if (mode == "ceaser"){
+
+        //recorrer cada letra de la palabra que se va a cifrar
+        for (let i = 0; i < text.length; i++) {
+
+             //codigo para cada letra
+            let c = text.charCodeAt(i);
+
+            // formula para cifrar. funciona con mayúsculas por el rango 65 a 90
+            if(c >= 65 && c <=  90) {
+               result += String.fromCharCode((c - 65 + shift) % 26 + 65);
+
+
+            }
+        }
+    }
+    return result;
+}
+
+function decrypt(text,shift){
+    var result = "";
+    shift = (26 - shift) % 26;
+    result = encrypt(text,shift);
+    return result;
+}
+*/
 window.cipher = {
-  // ... 
-};
+  encode: function(string, offset) {
+    return "textoCifrado"
+  }
+},
+{
+  decode: function(string, offset) {
+    return "textoDescifrado"
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,35 @@
+// acá debes escuchar eventos del DOM, invocar `cipher.encode()`
+//  o `cipher.decode()` según sea necesario y actualizar el resultado en la UI.
+
+
+ /*let encrypt = document.getElementById("mostrarCifrado");
+let decrypt = document.getElementById("mostrarDescifrado");
+
+//escuchar eventos
+encrypt.addEventListener("click", function() {console.log(window.cipher.encode())});
+decrypt.addEventListener("click", function() {console.log(window.cipher.decode())});
+/*mostrarCifrado.addEventListener("click", function()
+{
+
+}
+document.getElementById("mostrarCifrado").addEventListener("click", function(){
+  let textoDelUsuario= document.getElementById("texto").value;
+  }
+*/
+ function regresar(){
+    first.style.display = "block";
+    second.style.display = "none";
+    third.style.display = "none";
+}
+    function encrypt(){
+  first.style.display = "none";
+  second.style.display = "block";
+  third.style.display = "none";
+
+
+}
+function decrypt(){
+  first.style.display = "none";
+  second.style.display = "block";
+  third.style.display = "none";
+}
